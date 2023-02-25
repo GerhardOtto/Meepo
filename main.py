d138b106018ef1b6078d49eeab9edf34694944e5
Gerhard Otto <gerrieotto123@gmail.com>
2023-02-25 16:41:15 +0200

    display gui

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def login():
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel(master=frame, text="Login System", text_font=("Arial", 24))
+label = customtkinter.CTkLabel(master=frame, text="Login System", font=("Arial", 24))
 label.pack(pady=12, padx=10)
 
 entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
