53e6e2c9546906548881767139613f9bc0f7121d
Gerhard Otto <gerrieotto123@gmail.com>
2023-02-26 14:42:31 +0200

    replace rdoButtons with segmentedButton

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
 
 root = customtkinter.CTk()
-root.geometry("1080x720")
+root.geometry("500x500")
 
 def closeProgram():
     exit()
@@ -20,44 +20,52 @@ label = customtkinter.CTkLabel(master=frame, text="File Encoder", font=("Arial",
 label.pack(pady=12, padx=10)
 
 #text boxes
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Text")
-entry1.pack(pady=12, padx=10)
-entry = customtkinter.CTkEntry(master=frame,width=320,height=100,corner_radius=10, placeholder_text="Hello")
-entry.place(relx=0.5, rely=0.5)
+#entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Text")
+#entry1.pack(pady=12, padx=10)
+#entry = customtkinter.CTkEntry(master=frame,width=320,height=100,corner_radius=10, placeholder_text="Hello")
+#entry.place(relx=0.5, rely=0.5)
 
 #close button
-button = customtkinter.CTkButton(master=frame, text="Close", command=closeProgram)
+button = customtkinter.CTkButton(master=frame, text="Close", command=closeProgram,width=120,height=32,border_width=1,corner_radius=8)
 button.pack(pady=12, padx = 10)
-button.place(relx=0.9, rely=0.95, anchor=tkinter.CENTER)
+button.place(relx=0.8, rely=0.95, anchor=tkinter.CENTER)
 
 #run button
 button = customtkinter.CTkButton(master=frame, text="Run", command=encode.ownAlgoEncoder,width=120,height=32,border_width=1,corner_radius=8)
-button.pack(pady=12, padx = 12)
-button.place(relx=0.1, rely=0.95, anchor=tkinter.CENTER)
+button.pack(pady=12, padx = 10)
+button.place(relx=0.2, rely=0.95, anchor=tkinter.CENTER)
 
 #progress bar
 progressbar = customtkinter.CTkProgressBar(master=frame,width=160,height=20,border_width=5)
 progressbar.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 progressbar.set(0.1)
 
-#radio button
-def print_var(*args):
-    xdLOL = radiobutton_var.get()
-    print(xdLOL)
+#segmented button
+def segmented_button_callback(value):
+    if (value == "Value 1"):
+        print("First button clicked")
+        segemented_button.set("Value 5")
+    if (value == "Value 2"):
+        print("Seccond button clicked")
+        segemented_button.set("Value 5")
+    if (value == "Value 3"):
+        print("Third button clicked")
+        segemented_button.set("Value 5")
+    if (value == "Value 4"):
+        print("Fourth button clicked")
+        segemented_button.set("Value 5")
+
+segemented_button = customtkinter.CTkSegmentedButton(master=frame,values=["Value 1", "Value 2", "Value 3", "Value 4"],command=segmented_button_callback)
+segemented_button.pack(padx=20, pady=10)
+segemented_button.place(relx=0.5, rely=0.4, anchor=tkinter.CENTER)
+
+#input dialog
+def button_click_event():
+    dialog = customtkinter.CTkInputDialog(text="Type in a key:", title="Custom Key")
+    print("Password:", dialog.get_input())
 
 
-radiobutton_var = tkinter.IntVar(value=1)
-radiobutton_1 = customtkinter.CTkRadioButton(master=root, text= "Test", variable=radiobutton_var, value=1, bg_color="transparent",border_width_checked=7)
-radiobutton_1.place(relx=0.1, x=110, y=200, anchor=tkinter.CENTER)
-
-radiobutton_2 = customtkinter.CTkRadioButton(master=root, text="test 2", variable=radiobutton_var, value=2,border_width_checked=7)
-radiobutton_2.place(relx=0.3, x=110, y=200, anchor=tkinter.CENTER)
-
-radiobutton_3 = customtkinter.CTkRadioButton(master=root, variable=radiobutton_var, value=3,border_width_checked=7)
-radiobutton_3.place(relx=0.5, x=110, y=200, anchor=tkinter.CENTER)
-
-radiobutton_4 = customtkinter.CTkRadioButton(master=root, variable=radiobutton_var, value=4,border_width_checked=7)
-radiobutton_4.place(relx=0.7, x=110, y=200, anchor=tkinter.CENTER)
-radiobutton_var.trace('w', print_var)
+button = customtkinter.CTkButton(root, text="Custom Key", command=button_click_event)
+button.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
 
 root.mainloop()
