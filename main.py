9400a7bccbe3b0fd394d8e3bc5f1af901c299f87
Gerhard Otto <gerrieotto123@gmail.com>
2023-02-26 11:53:09 +0200

    add radiobuttons

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import customtkinter
 import tkinter
 import encode
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")
 
 root = customtkinter.CTk()
 root.geometry("1080x720")
@@ -31,7 +31,7 @@ button.pack(pady=12, padx = 10)
 button.place(relx=0.9, rely=0.95, anchor=tkinter.CENTER)
 
 #run button
-button = customtkinter.CTkButton(master=frame, text="notLogin", command=encode.ownAlgoEncoder,width=120,height=32,border_width=1,corner_radius=8)
+button = customtkinter.CTkButton(master=frame, text="Run", command=encode.ownAlgoEncoder,width=120,height=32,border_width=1,corner_radius=8)
 button.pack(pady=12, padx = 12)
 button.place(relx=0.1, rely=0.95, anchor=tkinter.CENTER)
 
@@ -39,5 +39,25 @@ button.place(relx=0.1, rely=0.95, anchor=tkinter.CENTER)
 progressbar = customtkinter.CTkProgressBar(master=frame,width=160,height=20,border_width=5)
 progressbar.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 progressbar.set(0.1)
+
+#radio button
+def print_var(*args):
+    xdLOL = radiobutton_var.get()
+    print(xdLOL)
+
+
+radiobutton_var = tkinter.IntVar(value=1)
+radiobutton_1 = customtkinter.CTkRadioButton(master=root, text= "Test", variable=radiobutton_var, value=1, bg_color="transparent",border_width_checked=7)
+radiobutton_1.place(relx=0.1, x=110, y=200, anchor=tkinter.CENTER)
+
+radiobutton_2 = customtkinter.CTkRadioButton(master=root, text="test 2", variable=radiobutton_var, value=2,border_width_checked=7)
+radiobutton_2.place(relx=0.3, x=110, y=200, anchor=tkinter.CENTER)
+
+radiobutton_3 = customtkinter.CTkRadioButton(master=root, variable=radiobutton_var, value=3,border_width_checked=7)
+radiobutton_3.place(relx=0.5, x=110, y=200, anchor=tkinter.CENTER)
+
+radiobutton_4 = customtkinter.CTkRadioButton(master=root, variable=radiobutton_var, value=4,border_width_checked=7)
+radiobutton_4.place(relx=0.7, x=110, y=200, anchor=tkinter.CENTER)
+radiobutton_var.trace('w', print_var)
 
 root.mainloop()
